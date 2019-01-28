4cd594fdca58b0359b194192947ff2b7fdef8da1
sajker <sajkerprog@gmail.com>
2019-01-29 00:08:39 +0300

    Math

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="training">
+    <h1>Math training</h1>
+    <hr>
+
+    <app-start-screen 
+      v-if="state == 'start'"
+      @onStart="onStart"
+    ></app-start-screen>
+
+    <app-question 
+      v-else-if="state == 'question'" 
+      @success = "onQuestionSuccess"
+      @error= "onQuestionError"
+    ></app-question>
+
+    <app-message 
+      v-else-if="state == 'message'"
+      :type="message.type"
+      :text="message.text"
+      ></app-message>
+    <app-result-screen v-else-if="state == 'result'"></app-result-screen>
+    <div v-else>Unknown state</div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      state: "start",
+      message: {
+        type: '',
+        text: ''
+      }
+    };
+  },
+  methods: {
+    onStart() {
+      this.state = "question";
+    },
+    onQuestionSuccess() {
+      this.state = 'message';
+      this.message.text = 'Good Job!';
+    },
+    onQuestionError(msg) {
+      this.state = 'message';
+      this.message.text = msg;
+    }
+  }
+};
+</script>
+<style type="scss" scoped>
+.training {
+  max-width: 700px;
+  margin: 20px auto;
+}
+</style>
+
+
+
---
+++ src/components/Message.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="alert">
+        <h2>{{text}}</h2>
+        <hr>
+        <button class="btn btn-primary">
+            Continue
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['type', 'text']
+}   
+</script>
+<style type="scss" scoped>
+.alert {
+    text-align: center;
+}
+h2, .btn {
+    margin: 20px auto;
+}
+</style>
